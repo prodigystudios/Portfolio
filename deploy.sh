d74eeb68e43bddbbd2f90fbc85325cfd81b2d10b
william ali <william.ali@hotmail.se>
2022-08-17 23:23:39 +0200

    b

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 #git push -f git@github.com:prodigystudios/prodigystudios.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:prodigystudios/portfolio.git main:gh-pages
+git push -f git@github.com:prodigystudios/portfolio.git master:gh-pages
 
 cd -
